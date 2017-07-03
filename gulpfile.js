a4762e20cdd4ee78fe5a6deeb818d584880a7c7d
leon in 't Veld <leon3110l@gmail.com>
2017-07-04 01:12:19 +0200

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const {spawn} = require("child_process");
+
+const electron = spawn("npm", ["start"]); // start electron
+electron.on("close", () => {
+  process.exit();
+});
+
+gulp.task('sass', function () {
+  return gulp.src('app/style/main.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('app/style/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('app/style/**/*.sass', ['sass']);
+});
