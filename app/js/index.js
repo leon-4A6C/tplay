ac93118c631c77c0d03e2489ecdfb5fd78a49352
leon in 't Veld <leon3110l@gmail.com>
2017-07-04 00:30:07 +0200

    commented some test stuff in index.js, because I don't need it at this moment, I guess

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,21 @@
-const tmdb = require("tmdbv3").init("81485988d49a76332eea5e3a5297d342");
-
-tmdb.search.movie("transformers", (err, res) => {
-  if (err) {
-    console.log(err);
-    return
-  }
-  tmdb.configuration((err, conf) => {
-    if (err) {
-      console.log(err);
-      return
-    }
-    // used to make the image urls
-    const configuration = conf;
-    console.log(configuration);
-    // const img = document.getElementById("img");
-    // img.src = configuration.images.base_url+"w500"+res.results[0].poster_path;
-    console.log(res);
-  });
-
-});
+// const tmdb = require("tmdbv3").init("81485988d49a76332eea5e3a5297d342");
+//
+// tmdb.search.movie("transformers", (err, res) => {
+//   if (err) {
+//     console.log(err);
+//     return
+//   }
+//   tmdb.configuration((err, conf) => {
+//     if (err) {
+//       console.log(err);
+//       return
+//     }
+//     // used to make the image urls
+//     const configuration = conf;
+//     console.log(configuration);
+//     // const img = document.getElementById("img");
+//     // img.src = configuration.images.base_url+"w500"+res.results[0].poster_path;
+//     console.log(res);
+//   });
+//
+// });
