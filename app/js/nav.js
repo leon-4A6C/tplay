9fee9ff2cb2490c21f631653d67fd7356cf4a633
leon in 't Veld <leon3110l@gmail.com>
2017-07-04 01:47:57 +0200

    sort of did stuff with the menu

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,5 +1,15 @@
-// fix the main being on top
 const topBar = document.getElementsByClassName("topBar")[0]; // there is only one
+const sideBar = document.getElementsByClassName("sideBar")[0]; // there is only one
 const main = document.getElementsByTagName("main")[0]; // there is only one
 
+// fix the main not being on the top or rather it being under the menu
 main.style.top = topBar.clientHeight + "px";
+
+
+
+// open or close sideBar/topBar
+// something on click or whatever
+document.getElementById("menuButton").addEventListener("click", e => {
+  sideBar.classList.toggle("sideBarClosed");
+  topBar.classList.toggle("topBarClosed");
+});
