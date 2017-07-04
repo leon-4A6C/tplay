36007f192b4ad1e6adb7a77f8faefd498274f841
leon in 't Veld <leon3110l@gmail.com>
2017-07-04 17:11:02 +0200

    added fonts better menu and added a loader thingy

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,6 +1,10 @@
 const topBar = document.getElementsByClassName("topBar")[0]; // there is only one
 const sideBar = document.getElementsByClassName("sideBar")[0]; // there is only one
 const main = document.getElementsByTagName("main")[0]; // there is only one
+const optionBar = document.getElementsByClassName("optionBar")[0]; // there is only one
+const pages = document.getElementsByClassName("contentWrapper");
+let currentPageOptionBar = optionBar.children[0]; // starts on tv-shows
+let currentPage = pages[0]; // starts on tv-shows
 
 // fix the main not being on the top or rather it being under the menu
 main.style.top = topBar.clientHeight + "px";
@@ -31,3 +35,35 @@ main.addEventListener("wheel", e => {
     main.style.paddingTop = e.deltaY+"px";
   }
 });
+
+for (var i = 0; i < optionBar.children.length; i++) {
+  optionBar.children[i].addEventListener("click", e => {
+    currentPageOptionBar.classList.remove("selected");
+    e.target.classList.add("selected");
+    currentPageOptionBar = e.target;
+    for (var i = 0; i < pages.length; i++) {
+      if (pages[i].classList.contains(e.target.innerHTML)) {
+        // move into view
+        shiftPages(pages[i]); // puts it in front of the thingy
+      }
+    }
+  });
+}
+
+// switches the page
+function shiftPages(newPage) {
+  if (newPage.classList.contains("tv-shows")) {
+    newPage.style.transform = "translateX(0)"; // tv-shows
+    pages[1].style.transform = "translateX(100%)"; // movies
+    pages[2].style.transform = "translateX(200%)"; // anime
+  } else if (newPage.classList.contains("movies")) {
+    newPage.style.transform = "translateX(0)"; // movies
+    pages[0].style.transform = "translateX(-100%)"; // tv-shows
+    pages[2].style.transform = "translateX(100%)"; // anime
+  } else if (newPage.classList.contains("anime")) {
+    newPage.style.transform = "translateX(0)"; // anime
+    pages[0].style.transform = "translateX(-200%)"; //tv-shows
+    pages[1].style.transform = "translateX(-100%)"; //movies
+  }
+  currentPage = newPage;
+}
