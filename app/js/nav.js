e65375bc90003603301b10643abc40841a258f0c
leon in 't Veld <leon3110l@gmail.com>
2017-07-04 14:09:51 +0200

    did stuff with the navigation

--- app/js/nav.js
+++ app/js/nav.js
@@ -11,5 +11,23 @@ main.style.top = topBar.clientHeight + "px";
 // something on click or whatever
 document.getElementById("menuButton").addEventListener("click", e => {
   sideBar.classList.toggle("sideBarClosed");
-  topBar.classList.toggle("topBarClosed");
+  if (!topBar.classList.contains("topBarClosed") && !sideBar.classList.contains("sideBarClosed")) {
+    topBar.classList.add("topBarClosed");
+    main.style.top = "0px";
+  } else if(topBar.classList.contains("topBarClosed") && sideBar.classList.contains("sideBarClosed")) {
+    topBar.classList.remove("topBarClosed");
+    main.style.top = topBar.clientHeight + "px";
+  }
+});
+
+main.addEventListener("wheel", e => {
+  if (e.wheelDelta > 0) {
+    topBar.classList.remove("topBarClosed");
+    main.style.top = topBar.clientHeight + "px";
+    main.style.paddingTop = "0";
+  } else if(!topBar.classList.contains("topBarClosed")) {
+    topBar.classList.add("topBarClosed");
+    main.style.top = "0px";
+    main.style.paddingTop = e.deltaY+"px";
+  }
 });
