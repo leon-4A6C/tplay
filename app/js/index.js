d063d1043f9188b1870fcd290da970908960b2fe
leon in 't Veld <leon3110l@gmail.com>
2017-07-06 01:20:26 +0200

    added some items

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,19 @@
 const TMDB = require("themoviedatabase");
 const tmdb = new TMDB("81485988d49a76332eea5e3a5297d342");
-
-Promise.all([
-  tmdb.jobs(),
-  tmdb.movies.videos(null, {movie_id: 297762})
-]).then(stuff => {
-  console.log(stuff);
+const baseImg = "https://image.tmdb.org/t/p/w500";
+tmdb.tv.popular().then(shows => {
+  for (var i = 0; i < shows.results.length; i++) {
+    pages[0].appendChild(generateItem(shows.results[i]));
+  }
 });
 
-tmdb.search.movies({query: "batman", year: 2007}).then(movies => {
-  console.log(movies);
-});
+function generateItem(data) {
+  let imgUrl = baseImg + data.poster_path;
+  let template = document.querySelector("#item");
+  let clone = document.importNode(template.content, true);
+  clone.title = data.name;
+  let img = clone.querySelector("img");
+  img.src = imgUrl;
+  img.alt = data.name;
+  return clone;
+}
