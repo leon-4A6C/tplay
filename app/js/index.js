fa31fd0716956f50db0569e6f8bcd7d90ace43d2
leon in 't Veld <leon3110l@gmail.com>
2017-07-05 17:36:53 +0200

    updated and tested themoviedatabase

--- app/js/index.js
+++ app/js/index.js
@@ -1,21 +1,13 @@
-// const tmdb = require("tmdbv3").init("81485988d49a76332eea5e3a5297d342");
-//
-// tmdb.search.movie("transformers", (err, res) => {
-//   if (err) {
-//     console.log(err);
-//     return
-//   }
-//   tmdb.configuration((err, conf) => {
-//     if (err) {
-//       console.log(err);
-//       return
-//     }
-//     // used to make the image urls
-//     const configuration = conf;
-//     console.log(configuration);
-//     // const img = document.getElementById("img");
-//     // img.src = configuration.images.base_url+"w500"+res.results[0].poster_path;
-//     console.log(res);
-//   });
-//
-// });
+const TMDB = require("themoviedatabase");
+const tmdb = new TMDB("81485988d49a76332eea5e3a5297d342");
+
+Promise.all([
+  tmdb.jobs(),
+  tmdb.movies.videos(null, {movie_id: 297762})
+]).then(stuff => {
+  console.log(stuff);
+});
+
+tmdb.search.movies({query: "batman", year: 2007}).then(movies => {
+  console.log(movies);
+});
