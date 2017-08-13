4efdfc55bf08bf916c073c1e9c1ece96c8d83554
leon in 't Veld <leon3110l@gmail.com>
2017-08-13 02:31:54 +0200

    adding react

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const babel = require('gulp-babel');
 const {spawn} = require("child_process");
 
 const electron = spawn("npm", ["start"]); // start electron
@@ -10,9 +11,31 @@ electron.on("close", () => {
 gulp.task('sass', function () {
   return gulp.src('app/style/main.sass')
     .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('app/style/'));
+    .pipe(gulp.dest('dist/style/'));
 });
 
-gulp.task('watch', ["sass"], function () {
-  gulp.watch('app/style/**/*.sass', ['sass']);
+gulp.task('js', function () {
+  return gulp.src('app/js/**/*.js')
+    .pipe(babel())
+    .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task("fonts", function() {
+  return gulp.src("app/style/fonts/**/*.*")
+  .pipe(gulp.dest("dist/style/fonts/"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("app/**/*.html")
+  .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('build', ["fonts", "sass", "js", "html"], function() {
+
+});
+
+gulp.task('watch', ["build"], function () {
+  gulp.watch('app/style/**/*.sass', ['build']);
+  gulp.watch('app/js/**/*.js', ['build']);
+  gulp.watch('app/**/*.html', ['build']);
 });
--- main.js
+++ main.js
@@ -5,6 +5,6 @@ let mainWindow;
 app.on("ready", () => {
   mainWindow = new BrowserWindow({width: 800, height: 800, autoHideMenuBar: true, title: "tplay"});
 
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadURL(`file://${__dirname}/dist/index.html`);
   mainWindow.openDevTools();
 });
