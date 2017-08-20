136df3f3b80b6235d5abac46f56d17df56590db0
leon in 't Veld <leon3110l@gmail.com>
2017-08-20 22:38:23 +0200

    made something more readable

--- app/js/components/layout.js
+++ app/js/components/layout.js
@@ -3,14 +3,16 @@ import React from 'react';
 import Info from "./info.js";
 import Page from "./page.js";
 // import Settings from "./settings.js";
-import Nav from "./nav.js";
+// import Nav from "./nav.js";
 import Player from "./player.js";
 
 export default class Layout extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      pages: [<Page key="tv" type="tv" name="tv-shows"></Page>]
+      pages: [
+        <Page key="tv" type="tv" name="tv-shows"></Page>
+      ]
     }
   }
   render() {
